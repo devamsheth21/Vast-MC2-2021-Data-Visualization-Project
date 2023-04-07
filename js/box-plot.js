6ff394c9e9e597c25fa1ba373ae35c143a0b489a
tirth-hihoriya <tirth.hihoriya.com>
2023-04-07 15:30:38 -0700

    box-plot-

--- js/box-plot.js
+++ js/box-plot.js
@@ -2,9 +2,9 @@
 
 
 
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 760 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+var margin = {top: 10, right: 30, bottom: 130, left: 70},
+    width = 1060 - margin.left - margin.right,
+    height = 1200 - margin.top - margin.bottom;
     
 document.addEventListener('DOMContentLoaded', function () {
 // append the svg object to the body of the page
@@ -18,7 +18,7 @@ var svg = d3.select("#box_plot_div")
 
 
 // Read the data and compute summary statistics for each specie
-d3.csv("MC2/cc_data.csv", function(data) {
+d3.csv("MC2/cc_data.csv").then( function(data) {
     console.log(data);
   // Compute quartiles, median, inter quantile range min and max --> these info are then used to draw the box.
   var sumstat = d3.rollup(data, function(d) {
@@ -66,10 +66,13 @@ d3.csv("MC2/cc_data.csv", function(data) {
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
   // Show the Y scale
   var y = d3.scaleLinear()
-    .domain([3,9])
+    .domain([0,10000])
     .range([height, 0])
   svg.append("g").call(d3.axisLeft(y))
 
@@ -79,23 +82,23 @@ d3.csv("MC2/cc_data.csv", function(data) {
     .data(sumstat)
     .enter()
     .append("line")
-      .attr("x1", function(d){return(x(d.key))})
-      .attr("x2", function(d){return(x(d.key))})
-      .attr("y1", function(d){return(y(d.value.min))})
-      .attr("y2", function(d){return(y(d.value.max))})
+      .attr("x1", function(d){return(x(d[0]))})
+      .attr("x2", function(d){return(x(d[0]))})
+      .attr("y1", function(d){return(y(d[1].min))})
+      .attr("y2", function(d){return(y(d[1].max))})
       .attr("stroke", "black")
       .style("width", 40)
 
   // rectangle for the main box
-  var boxWidth = 100
+  var boxWidth = 30
   svg
     .selectAll("boxes")
     .data(sumstat)
     .enter()
     .append("rect")
-        .attr("x", function(d){return(x(d.key)-boxWidth/2)})
-        .attr("y", function(d){return(y(d.value.q3))})
-        .attr("height", function(d){return(y(d.value.q1)-y(d.value.q3))})
+        .attr("x", function(d){return(x(d[0])-boxWidth/2)})
+        .attr("y", function(d){return(y(d[1].q3))})
+        .attr("height", function(d){return(y(d[1].q1)-y(d[1].q3))})
         .attr("width", boxWidth )
         .attr("stroke", "black")
         .style("fill", "#69b3a2")
@@ -106,15 +109,15 @@ d3.csv("MC2/cc_data.csv", function(data) {
     .data(sumstat)
     .enter()
     .append("line")
-      .attr("x1", function(d){return(x(d.key)-boxWidth/2) })
-      .attr("x2", function(d){return(x(d.key)+boxWidth/2) })
-      .attr("y1", function(d){return(y(d.value.median))})
-      .attr("y2", function(d){return(y(d.value.median))})
+      .attr("x1", function(d){return(x(d[0])-boxWidth/2) })
+      .attr("x2", function(d){return(x(d[0])+boxWidth/2) })
+      .attr("y1", function(d){return(y(d[1].median))})
+      .attr("y2", function(d){return(y(d[1].median))})
       .attr("stroke", "black")
       .style("width", 80)
 
 // Add individual points with jitter
-var jitterWidth = 50
+var jitterWidth = 20
 svg
   .selectAll("indPoints")
   .data(data)
@@ -122,7 +125,7 @@ svg
   .append("circle")
     .attr("cx", function(d){return(x(d.location) - jitterWidth/2 + Math.random()*jitterWidth )})
     .attr("cy", function(d){return(y(d.price))})
-    .attr("r", 4)
+    .attr("r", 3)
     .style("fill", "white")
     .attr("stroke", "black")
 
